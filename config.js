6b19d7c30b269bb23fd34f375b3474f5efdb81cc
Josh Stoltenberg <stoltenberg96@gmail.com>
2019-04-14 20:32:23 -0500

    api response filtered

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var api_base_url = "https://api.frankerfacez.com/v1/";
 var get_emotes_url =
   api_base_url + "emoticons?sort=count-desc?per_page=2000?_sceheme=https";
-var all_emotes;
+var all_emotes = new Array();
 let checkApi = document.getElementById("checkApi");
 
 checkApi.onclick = function() {
@@ -9,14 +9,16 @@ checkApi.onclick = function() {
 
   request.open("GET", get_emotes_url), true;
   request.send();
-  console.log(request.response.JSON);
   request.onload = function() {
     var response = JSON.parse(this.response);
     console.log(response);
 
     response = response.emoticons;
 
-    response.forEach(emote => {
+    response.forEach(entry => {
+      var emote = new Object();
+      emote.name = entry.name;
+      emote.links = entry.urls;
       all_emotes.push(emote);
     });
     console.log(all_emotes);
